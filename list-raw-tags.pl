037d41c6ab5dee374330d4c9b4333e75ac1a1b0f
jvverde <jvverde@outlook.com>
2023-02-22 00:21:32 +0000

    new tool and list raw tags script

---
+++ list-raw-tags.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use Getopt::Long;
+use Flickr::API;
+use Data::Dumper;
+binmode(STDOUT, ':utf8');
+
+$\ = "\n";
+
+sub usage {
+    print "This script lists all tags of the current user's photos.";
+    print "Usage: $0 [OPTIONS]";
+    print "Options:";
+    print "  -h, --help    Show this help message and exit";
+    print "\nNOTE: It assumes the user's tokens are initialized in the file '$ENV{HOME}/saved-flickr.st'";
+    exit;
+}
+
+GetOptions(
+    'h|help' => \&usage
+);
+
+my $config_file = "$ENV{HOME}/saved-flickr.st";
+my $flickr = Flickr::API->import_storable_config($config_file);
+
+my $response = $flickr->execute_method('flickr.tags.getListUserRaw');
+
+die "Error retrieving tags: $response->{error_message}\n\n" unless $response->{success};
+
+my $tags = $response->as_hash()->{who}->{tags}->{tag};
+
+foreach my $tag (@$tags) {
+  my $ref = ref $tag->{raw};
+  if ($ref eq 'HASH') {
+    print  $tag->{clean};
+    next;
+  } elsif ($ref eq 'ARRAY') {
+    print foreach grep { !ref $_ } (@{$tag->{raw}})
+  } elsif (!$ref) {
+    print $tag->{raw};
+  } else {
+    print Dumper $tag->{raw};
+  }
+}
